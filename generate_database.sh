1897103546c6c59edc09fc93b13b88ac2091e4a7
PaulDarius <sarmadipauldarius@gmail.com>
2016-02-08 16:50:02 +0700

    optional demo mode

--- generate_database.sh
+++ generate_database.sh
@@ -2,6 +2,11 @@
 
 mkdir Database;
 if [ $# -eq 0 ]; then
-	echo "Expected argument: Path to the database."
-else find $1/ -iname '*.avi' -exec ./bin/FaceDetector {} \;
+	echo "Expected argument: Path to the database.";
+	echo "Optional argument: 1 for demo. 0 for default ie no demo"
+elif [ $# -eq 1 ]; then
+	find $1/ -iname '*.avi' -exec ./bin/FaceDetector {} 0 \;
+elif [ $# -eq 2 ]; then
+	find $1/ -iname '*.avi' -exec ./bin/FaceDetector {} $2 \;
 fi
+echo -e "All the videos have been processed. The generation of the database is complete!\n";
--- src/FaceDetector.cpp
+++ src/FaceDetector.cpp
@@ -13,7 +13,7 @@ double min_face_size=10;
 double max_face_size=300;
 
  /** Function Headers */
- Mat detectAndDisplay( Mat frame, string window_name, int frame_number, string video_string);
+ Mat detectAndDisplay( Mat frame, string window_name, int frame_number, string video_string, int demo_mode);
  string NumberToString ( int Number );
 
  /** Global variables */
@@ -25,17 +25,19 @@ double max_face_size=300;
 
 void help()
 {
-	cout << "This software is supposed to be use indirectly, within the script generate_database.sh. Please read the readme file." << endl;
+	cout << "This software is supposed to be used indirectly, within the script generate_database.sh. Please read the readme file." << endl;
 }
 
 int main(int argc, char* argv[])
 {
-	if (argc != 2)
+	if (argc < 2)
 	{
 		help();
 		return -1;
 	}
 	string video_string_with_slashes = argv[1];
+	int demo_mode = atoi(argv[2]);
+	
 	string video_string=video_string_with_slashes;
 	for (int i=0; i<video_string.size();i++)
 	{
@@ -47,46 +49,52 @@ int main(int argc, char* argv[])
 	if( !profile_cascade.load( profile_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };	
 
     VideoCapture cap(video_string_with_slashes);
-    if(!cap.isOpened())  // check if we succeeded
+    if(!cap.isOpened())
         return -1;
 
     Mat edges;
+    if (demo_mode)
+    {
     namedWindow("Result",WINDOW_NORMAL);
 	namedWindow("Initial",WINDOW_NORMAL);
+	}
 	VideoWriter outputVideo;
 	
 	string command1 = "mkdir Database/"+video_string+";";
-	string command2 = "mkdir Database/"+video_string+"/video;";
 	system(command1.c_str());
-	system(command2.c_str());
-
-
-	const string VIDEO_NAME = "Database/"+video_string+"/video/"+"detected.avi";
+    if (demo_mode)
+    {
+		string command2 = "mkdir Database/"+video_string+"/video;";	
+		system(command2.c_str());
+		const string VIDEO_NAME = "Database/"+video_string+"/video/"+"detected_faces.avi";
+		int ex = static_cast<int>(cap.get(CV_CAP_PROP_FOURCC));      
+		double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
+		double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
+		Size frameSize(static_cast<int>(dWidth), static_cast<int>(dHeight));
+		outputVideo.open(VIDEO_NAME, ex /*CV_FOURCC('P','I','M','1') FOR WEBCAM PURPOSE */ /* Use ex if I am using a file instead of webcam*/, cap.get(CV_CAP_PROP_FPS), frameSize, true);
+	}
 	
-	
-	int ex = static_cast<int>(cap.get(CV_CAP_PROP_FOURCC));
-	       
-    double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-    double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
-    Size frameSize(static_cast<int>(dWidth), static_cast<int>(dHeight));
-
-	outputVideo.open(VIDEO_NAME, ex /*CV_FOURCC('P','I','M','1') FOR WEBCAM PURPOSE */ /* Use ex if I am using a file instead of webcam*/, cap.get(CV_CAP_PROP_FPS), frameSize, true);
 	Mat frame;
 	int frame_number = 1;
+	Mat outputFrame;
+	cout << video_string << "'s face detection engaged. Be patient, the process may take from several minutes to few hours.\n" << endl;
     while(1)
     {
         cap >> frame; // get a new frame from camera
-        imshow("Initial", frame);
-        outputVideo.write(detectAndDisplay(frame, "Result", frame_number, video_string));
+        if (demo_mode)
+			imshow("Initial", frame);
+		outputFrame = detectAndDisplay(frame, "Result", frame_number, video_string, demo_mode);
+        if (demo_mode)
+			outputVideo.write(outputFrame);
         if(waitKey(10) >= 0) break;
         frame_number++;
     }
-    cout << "Done" << endl;
-    // the camera will be deinitialized automatically in VideoCapture destructor
+
+	cout << video_string << "is Done.\n" << endl;
     return 0;
 }
 
-Mat detectAndDisplay( Mat frame, string window_name, int frame_number, string video_string)
+Mat detectAndDisplay( Mat frame, string window_name, int frame_number, string video_string, int demo_mode )
 {
   std::vector<Rect> faces;
   std::vector<Rect> faces_profile;
@@ -104,7 +112,8 @@ Mat detectAndDisplay( Mat frame, string window_name, int frame_number, string vi
 		Rect_ROI = Mat(frame,faces[i]);
 		filename = "Database/"+video_string+"/"+"Frame"+NumberToString(frame_number)+"Face"+NumberToString(i)+".jpg";
 		imwrite(filename,Rect_ROI);
-		rectangle(frame, faces[i],Scalar( 255, 0, 255 ),3,8,0);
+		if (demo_mode)
+			rectangle(frame, faces[i],Scalar( 255, 0, 255 ),3,8,0);
 	}
 
    for( int i = 0; i < faces_profile.size(); i++ )
@@ -112,16 +121,18 @@ Mat detectAndDisplay( Mat frame, string window_name, int frame_number, string vi
 		Rect_ROI = Mat(frame,faces[i]);
 		filename = "Database/"+video_string+"/"+"|Profile|Frame"+NumberToString(frame_number)+"Face"+NumberToString(i)+".jpg";
 		imwrite(filename,Rect_ROI);
-		rectangle(frame, faces_profile[i],Scalar( 255, 0, 255 ),3,8,0);
+		if (demo_mode)
+			rectangle(frame, faces_profile[i],Scalar( 255, 0, 255 ),3,8,0);
 	}
   //-- Show what you got
-  imshow( window_name, frame );
+  if (demo_mode)
+	imshow( window_name, frame );
   return frame;
- }
+}
  
- string NumberToString ( int Number )
-{
+string NumberToString ( int Number )
+	{
 	stringstream ss;
 	ss << Number;
 	return ss.str();
-}
+	}
