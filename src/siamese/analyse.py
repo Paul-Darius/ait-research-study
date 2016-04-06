f6c170cac5ffaf735ebfed0304d1f3931f0e13e4
Pauldarius <sarmadipauldarius@gmail.com>
2016-04-06 21:35:28 +0700

    Analysis

--- src/siamese/analyse.py
+++ src/siamese/analyse.py
@@ -8,8 +8,8 @@ caffe.set_device(0)
 caffe.set_mode_gpu()
 net = caffe.Net('src/siamese/siamese_mbk.prototxt', 'src/siamese/siamese_mbk/_iter_50000.caffemodel', caffe.TEST);
 
-test1 = open("Caffe_Files/test1.txt","r")
-test2 = open("Caffe_Files/test2.txt","r")
+train1 = open("Caffe_Files/train1.txt","r")
+train2 = open("Caffe_Files/train2.txt","r")
 
 #After the while loop
 # Contains the x,y coordinates of the first image
@@ -20,11 +20,11 @@ out2=[]
 energies=[]
 # expected[k] contains 1 if the images in line k of test1.txt and test2.txt represent the same person and 0 else.
 expected=[]
-
+print "The accuracy computation has started. The process is long. Please be patient."
 while 1:
-	txt1 = test1.readline()
-	txt2 = test2.readline()
-	if txt1='' or txt2='':
+	txt1 = train1.readline()
+	txt2 = train2.readline()
+	if txt1=='' or txt2=='':
 		break
 	source1,_,label1=txt1.rpartition(' ')
 	source2,_,label2=txt2.rpartition(' ')
@@ -38,14 +38,12 @@ while 1:
 	net.blobs['data'].data[...] = im2
 	output2 = net.forward()
 	out2.append(output2['feat'])
-	print output1['feat']
-	print output2['feat']
 	expected.append(int(label1))
-	energy = sqrt((output1*output1+output2*output2))
+	energy = (float)((output2['feat'][0][0]-output1['feat'][0][0])**2+(output1['feat'][0][1]-output2['feat'][0][1])**2)**(0.5)
 	energies.append(energy)
 
-test1.close()
-test2.close()
+train1.close()
+train2.close()
 # If the energy is above the threshold then the two images are considered as different. If the energy is under the threshol, they are identic.
 
 threshold = 0.
@@ -57,7 +55,7 @@ barycenter2 = 0.
 size1 = 0
 
 for i in range(0,len(energies)-1):
-	if expected[k] = 0:
+	if expected[k] == 0:
 		barycenter1 += energies[k]
 		size1+=1
 	else:
@@ -76,13 +74,13 @@ false_pos = 0
 falseneg = 0
 
 for i in range(0,len(energies)-1):
-	if (energy[i]>threshold and expected[i]==0)
+	if (energy[i]>threshold and expected[i]==0):
 		true_neg+=1
-	else if (energy[i]>threshold and expected[i]==1):
+	elif (energy[i]>threshold and expected[i]==1):
 		false_neg+=1
-	else if (energy[i]<=threshold and expected[i]==1):
+	elif (energy[i]<=threshold and expected[i]==1):
 		true_pos+=1
-	else if (energy[i]<=threshold and expected[i]==0):
+	elif (energy[i]<=threshold and expected[i]==0):
 		false_pos+=1
 
 # We finally give the result
